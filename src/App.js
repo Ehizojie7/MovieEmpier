fedf8102fdd0d350ea4a98c9dec821cb2a08858e
Ehizojie Matthias <ehizzgabriel@gmail.com>
2023-01-21 00:57:06 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import MovieCard from './MovieCard';
+import { useEffect,  useState } from 'react';
+import './index.css';
+import SearchIcon from './search.svg';
+import Footer from './Footer';
+
+const API_URL = "https://www.omdbapi.com?apikey=f75d18f2";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Avengers");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  return (
+    <div className="app">
+      <h1>MovieEmpier</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div className='footer'>
+            <h3 style={{margin: '30px'}}> Design made by Ehizojie</h3>
+            <p style={{marginBottom: '40px'}}>&copy; 2023 All rights reserved.</p>
+        </div>
+    );
+};
+
+export default Footer;
